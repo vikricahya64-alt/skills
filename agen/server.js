5d37f5d420fedc7bfa92ea6c62707efea02eb3d8
vikricahya64-alt <vikricahya64@gmail.com>
2026-08-30 12:42:20 +0700

    feat: tombol jeda (batalkan proses berjalan via AbortController) + input suara ke teks (Web Speech API id-ID) (3.4.3)

--- agen/server.js
+++ agen/server.js
@@ -494,7 +494,7 @@ app.get("/api/info", async (req, res) => {
     limitFileMB: 20,
     maxQuestion: MAX_LEN,
     uptime: Math.round(process.uptime()),
-    version: "3.4.1",
+    version: "3.4.3",
     kb: true,
     kbCards: KB.loadCards().length,
     maxTopSkills: 3,
